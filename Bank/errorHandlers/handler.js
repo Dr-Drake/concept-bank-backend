4c32b467b716f5bdf616f6472eb84814ec63cd45
Ikem Ezechukwu <tommy_flo@live.com>
2020-01-17 14:05:49 +0000

    Fixed account creation issue again

--- Bank/errorHandlers/handler.js
+++ Bank/errorHandlers/handler.js
@@ -4,10 +4,7 @@ function handleError(err, res) {
 
    // console.log(`ERROR!: ${err.status}`)
     res.status(err.status)
-    res.render('error', {
-        message: err.message,
-        error: err
-    })
+    
 }   
 
 
